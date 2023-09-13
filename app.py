7bb4f7ab2a11c59aa1a41b68b3e0d6adca877ae9
cswope <cswope@evolytics.com>
2023-09-13 17:57:20 -0500

    Added the jokes.

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+jokes = [
+    "Which body part does a programmer know best? ARM",
+    "How do you get the code for the bank vault? You checkout their branch."
+]
+
 @app.get("/")
-def hello_world():
-    return "<p>Hello, World! Corey!</p>"
+def tell_a_joke():
+    joke = random.choice(jokes)
+    return f"<p>{joke}</p>"
