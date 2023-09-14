f303e9055be7b8e07bf1c8976d398ebc99e88435
cswope <cswope@evolytics.com>
2023-09-14 09:27:11 -0500

    Finished data layer refactoring

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import flask
 from flask import Flask
+from data import jokes
 import random
 
 app = Flask(__name__)
-
-jokes = [
-    "Which body part does a programmer know best? ARM",
-    "How do you get the code for the bank vault? You checkout their branch."
-]
 
 @app.get("/")
 def tell_a_joke():
---
+++ data.py
@@ -0,0 +1,4 @@
+jokes = [
+    "Which body part does a programmer know best? ARM",
+    "How do you get the code for the bank vault? You checkout their branch."
+]
